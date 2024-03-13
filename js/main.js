196aebbcfbdc79263bfe833971a58d6cb8d90d61
wataru0531 <obito0531@gmail.com>
2024-03-13 15:32:45 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+import "../sass/style.scss";
+
+/**************************************************************
+// ScrollMagic
+// 参考 
+  https://www.npmjs.com/package/scrollmagic
+  https://www.npmjs.com/package/scrollmagic-plugins
+
+***************************************************************/
